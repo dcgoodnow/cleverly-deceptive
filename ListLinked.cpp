1d6cd194992cd8bd4e71fa2cd30418332ad8de60
Daniel Goodnow <danielgoodnow@gmail.com>
2014-01-23 04:07:33 +0000

    add a few more functions

--- ListLinked.cpp
+++ ListLinked.cpp
@@ -72,3 +72,49 @@ void List::remove() throw (logic_error)
    }
    return;
 }
+
+template <typename DataType>
+void List::replace(const DataType& newDataItem) throw (logic_error)
+{
+   if( !isEmpty() )
+   {
+      cursor->data = newDataItem;
+   }
+   return;
+}
+
+template <typename DataType>
+void List::clear()
+{
+   if( !isEmpty() )
+   {
+      cursor = head;
+      ListNode* nodeTmp = cursor;
+      cursor = cursor->next;
+      delete nodeTmp;
+      while(cursor != NULL)
+      {
+         nodeTmp = cursor;
+         cursor = cursor->next;
+         delete nodeTmp;
+      }
+      head = NULL;
+   }
+   return;
+}
+
+template <typename DataType>
+bool List:isEmpty() const
+{
+   if( head == NULL )
+   {
+      return true;
+   }
+   return false;
+}
+
+template <typename DataType>
+bool List::isFull()
+{
+   return false;
+}
