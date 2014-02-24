9f162b1aae2a9f5960b7767c773e98e41f8720bd
Battlingtoads <danielgoodnow@gmail.com>
2014-02-24 12:45:18 -0800

    move functions and readin functionality

--- pc2/rushhour.cpp
+++ pc2/rushhour.cpp
@@ -2,3 +2,94 @@
  * @file rushhour.cpp
  * @author Daniel Goodnow
 */
+
+#include <iostream>
+
+using namespace std;
+
+
+struct car{
+	int xPos;
+	int yPos;
+	int length;
+	char orientation;
+};
+
+bool moveForward(car*, int);
+
+bool moveBackward(car*, int);
+	
+int main()
+{
+	int size;
+	cin >> size;
+	car* grid = new car[size];
+	for( int i = 0; i < size; i++)
+	{
+		cin >> grid[i].length;
+		cin >> grid[i].orientation;
+		cin >> grid[i].xPos;
+		cin >> grid[i].yPos;
+	}
+
+}
+
+bool moveForward(car* cars, int carNumber)
+{
+	switch(cars[carNumber].orientation)
+	{
+		case 'H':
+			if(cars[carNumber].xPos + cars[carNumber].length != 7)
+			{
+				cars[carNumber].xPos++;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			break;
+
+		case 'V':
+			if(cars[carNumber].yPos + cars[carNumber].length != 7)
+			{
+				cars[carNumber].yPos++;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			break;
+	}
+}
+
+bool moveBackward(car* cars, int carNumber)
+{
+	switch(cars[carNumber].orientation)
+	{
+		case 'H':
+			if(cars[carNumber].xPos != 0)
+			{
+				cars[carNumber].xPos--;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			break;
+
+		case 'V':
+			if(cars[carNumber].yPos != 0)
+			{
+				cars[carNumber].yPos--;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			break;
+	}
+}
