264d58c2802145737b51174466f2dc71a7ddb69b
Battlingtoads <danielgoodnow@gmail.com>
2014-03-04 14:34:23 -0800

    I have no idea what i'm doing

--- lab8/ExpressionTree.cpp
+++ lab8/ExpressionTree.cpp
@@ -13,17 +13,39 @@ void ExprTree<DataType>::build()
 	cin >> input;
 	root = new ExprTreeNode(input, NULL, NULL);
 	cin >> input;
-	buildBranch(input, root->left);
+	buildBranch(input, root);
 }
 
 template <typename DataType>
 void ExprTree<DataType>::buildBranch(char input, ExprTreeNode* parent)
 {
 	parent = new ExprTreeNode(input, NULL, NULL);
+	char nextIn;
+	cin >> nextIn;
 	if(input == '+' || input == '-' || input == '*' || input == '/')
 	{
-		char nextIn;
-		cin >> nextIn;
 		buildBranch(nextIn, parent->left);
 	}
+	else
+	{
+		buildBranch(nextIn, parent->right);
+	}
+}
+
+template <typename DataType>
+void ExprTree<DataType>::expression() const
+{
+	ExprTreeNode* nodeTmp = root;
+	while(root->left != NULL)
+	{
+		root = root->left;
+	}
+	cout << root->dataItem;
+	
+}
+
+template <typename DataType>
+void ExprTree<DataType>::clear()
+{
+
 }
