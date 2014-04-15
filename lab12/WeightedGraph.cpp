bbd8363b961dbd913f9537101a96329e45b6b4d1
Daniel <danielgoodnow@gmail.com>
2014-04-15 15:07:44 -0700

    almost all working

--- lab12/WeightedGraph.cpp
+++ lab12/WeightedGraph.cpp
@@ -5,7 +5,57 @@ WeightedGraph::WeightedGraph(int maxNumber)
 	maxSize = maxNumber;
 	size = 0;
 	adjMatrix = new int[maxSize*maxSize];
+	pathMatrix = new int[maxSize*maxSize];
 	vertexList = new Vertex[maxSize];
+}
+
+WeightedGraph::~WeightedGraph()
+{
+	size = 0;
+	maxSize = 0;
+	delete[] vertexList;
+	delete[] adjMatrix;
+}
+
+WeightedGraph::WeightedGraph(const WeightedGraph& other)
+{
+	maxSize = other.maxSize;
+	size = other.size;
+	adjMatrix = new int[maxSize*maxSize];
+	vertexList = new Vertex[maxSize];
+	for(int i = 0; i < size; i++)
+	{
+		vertexList[i] = other.vertexList[i];
+		for(int j = 0; j < size; j++)
+		{
+			adjMatrix[toArr(i,j)] = other.adjMatrix[toArr(i,j)];
+		}
+	}
+}
+
+WeightedGraph& WeightedGraph::operator=(const WeightedGraph& other)
+{
+	if(this == &other)
+	{
+		return *this;
+	}
+	clear();
+
+	maxSize = other.maxSize;
+	size = other.size;
+	delete[] adjMatrix;
+	delete[] vertexList;
+	adjMatrix = new int[maxSize*maxSize];
+	vertexList = new Vertex[maxSize];
+	for(int i = 0; i < size; i++)
+	{
+		vertexList[i] = other.vertexList[i];
+		for(int j = 0; j < size; j++)
+		{
+			adjMatrix[toArr(i,j)] = other.adjMatrix[toArr(i,j)];
+		}
+	}
+	return *this;
 }
 
 void WeightedGraph::insertVertex(const Vertex& newVertex) throw (logic_error)
@@ -50,10 +100,12 @@ bool WeightedGraph::retrieveVertex(const string& v, Vertex& vData) const
 		return false;
 	}
 	int i = 0;
-	while(i < size && v != vertexList[i].getLabel()) //fix these conditionals
+	while(i < size-1 && v != vertexList[i].getLabel()) //fix these conditionals
 	{
 		i++;
+		cout << i << endl;
 	}
+	
 	if(v == vertexList[i].getLabel())
 	{
 		vData = vertexList[i];
@@ -118,6 +170,31 @@ void WeightedGraph::removeVertex(const string& v) throw (logic_error)
 	{
 		adjMatrix[toArr(t, i)] = adjMatrix[toArr(t+1, i)];
 	}
+	size--;
+}
+
+void WeightedGraph::removeEdge(const string& v1, const string& v2) throw (logic_error)
+{
+	if(isEmpty() || size == 1)
+	{
+		return;
+	}
+	Vertex temp, temp2;
+	if(!retrieveVertex(v1, temp) || !retrieveVertex(v2, temp2))
+	{
+		return;
+	}
+	int rNum=0, cNum = 0;
+	while(temp.getLabel() != vertexList[rNum].getLabel())
+	{
+		rNum++;
+	}
+	while(temp2.getLabel() != vertexList[cNum].getLabel())
+	{
+		cNum++;
+	}
+	adjMatrix[toArr(rNum,cNum)] = INFINITE_EDGE_WT;
+	adjMatrix[toArr(cNum,rNum)] = INFINITE_EDGE_WT;
 }
 
 bool WeightedGraph::isEmpty() const
@@ -136,4 +213,132 @@ bool WeightedGraph::isFull() const
 		return true;
 	}
 	return false;
+}
+
+void WeightedGraph::clear()
+{
+	size = 0;
+}
+
+void WeightedGraph::showShortestPaths() const
+{
+	getPaths();
+#if LAB12_TEST2
+	cout << vertexList[row].color << endl;
+#endif
+
+	cout << endl << "Path matrix : " << endl << '\t';
+	for ( int col = 0 ; col < size ; col++ )
+		cout << col << '\t';
+	cout << endl;
+	for ( int row = 0 ; row < size ; row++ )
+	{
+		cout << row << '\t';
+		for ( int col = 0 ; col < size ; col++ )
+		{
+			int wt = pathMatrix[toArr(row,col)];
+			if ( wt == INFINITE_EDGE_WT)
+				cout << "- \t";
+			else
+				cout << wt << '\t';
+		}
+		cout << endl;
+	}
+}
+
+void WeightedGraph::getPaths() const
+{
+	if(isEmpty()){return;}
+	for(int i = 0; i<size; i++)
+	{
+		for(int j = 0; j<size; j++)
+		{
+			pathMatrix[toArr(i,j)] = adjMatrix[toArr(i,j)];
+		}
+	}
+
+	for(int i = 0; i < size; i++)
+	{
+		pathMatrix[toArr(i,i)] = 0;
+	}
+
+	for(int j = 0; j<size; ++j)
+	{
+		for(int k = 0; k<size; ++k)
+		{
+			for(int m = 0; m<size; ++m)
+			{
+				if(adjMatrix[toArr(j,m)] != INFINITE_EDGE_WT &&
+					adjMatrix[toArr(m,k)] != INFINITE_EDGE_WT &&
+					adjMatrix[toArr(j,m)] + adjMatrix[toArr(m,k)] < pathMatrix[toArr(j,k)])
+				{
+					pathMatrix[toArr(j,k)] = adjMatrix[toArr(j,m)]+adjMatrix[toArr(m,k)];
+				}
+			}
+		}
+	}
+}
+int WeightedGraph::getEdge(int r, int c) const
+{
+	return adjMatrix[toArr(r,c)];
+}
+
+void WeightedGraph::showStructure () const
+
+// Outputs a graph's vertex list and adjacency matrix. This operation
+// is intended for testing/debugging purposes only.
+
+{
+    if ( size == 0 ) {
+       cout << "Empty graph" << endl;
+    } else {
+       cout << endl << "Vertex list : " << endl;
+       for ( int row = 0 ; row < size ; row++ )
+           cout << row << '\t' << vertexList[row].getLabel()
+#if LAB12_TEST2
+		<< vertexList[row].color
+#endif
+		<< endl;
+
+       cout << endl << "Edge matrix : " << endl << '\t';
+       for ( int col = 0 ; col < size ; col++ )
+           cout << col << '\t';
+       cout << endl;
+       for ( int row = 0 ; row < size ; row++ )
+       {
+           cout << row << '\t';
+           for ( int col = 0 ; col < size ; col++ )
+           {
+               int wt = getEdge(row,col);
+               if ( wt == INFINITE_EDGE_WT)
+                  cout << "- \t";
+               else
+                  cout << wt << '\t';
+           }
+           cout << endl;
+       }
+    }
+}
+
+bool WeightedGraph::hasProperColoring() const
+{
+	if(isEmpty()){return true;}
+	
+	for(int i = 0; i < size; i++)
+	{
+		for(int j=0; j < size; j++)
+		{
+			if(adjMatrix[toArr(i,j)] != INFINITE_EDGE_WT &&
+				vertexList[i].getColor() == vertexList[j].getColor())
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool WeightedGraph::areAllEven() const
+{
+	return true;
 }
--- lab12/WeightedGraph.h
+++ lab12/WeightedGraph.h
@@ -19,78 +19,80 @@ using namespace std;
 
 class WeightedGraph
 {
-  public:
-    static const int 
-	MAX_GRAPH_SIZE = 10,         // Default number of vertices
-	INFINITE_EDGE_WT = INT_MAX;  // "Weight" of a missing edge
+	public:
+		static const int 
+			MAX_GRAPH_SIZE = 10,         // Default number of vertices
+								INFINITE_EDGE_WT = INT_MAX;  // "Weight" of a missing edge
 
-    class Vertex
-    {
-      public:
-	void setLabel( const string& newLabel ) { label=newLabel; }
-	string getLabel( ) const { return label; }
-	void setColor(char newColor) { color = newColor; }
-	char getColor() const { return color; }
+		class Vertex
+		{
+			public:
+				void setLabel( const string& newLabel ) { label=newLabel; }
+				string getLabel( ) const { return label; }
+				void setColor(char newColor) { color = newColor; }
+				char getColor() const { return color; }
 
-      private:
-	string label;   // Vertex label
-	char color;
-    };
+			private:
+				string label;   // Vertex label
+				char color;
+		};
 
-    //--------------------------------------------------------------------
+		//--------------------------------------------------------------------
 
-    // Constructors
-    WeightedGraph ( int maxNumber = MAX_GRAPH_SIZE );
-    WeightedGraph ( const WeightedGraph& other );
-    // Overloaded assignment operator
-    WeightedGraph& operator= ( const WeightedGraph& other );
+		// Constructors
+		WeightedGraph ( int maxNumber = MAX_GRAPH_SIZE );
+		WeightedGraph ( const WeightedGraph& other );
+		// Overloaded assignment operator
+		WeightedGraph& operator= ( const WeightedGraph& other );
 
-    // Destructor
-    ~WeightedGraph ();
+		// Destructor
+		~WeightedGraph ();
 
-    // Graph manipulation operations
-    void insertVertex ( const Vertex& newVertex )            // Insert vertex
-        throw ( logic_error );
-    void insertEdge ( const string& v1, const string& v2, int wt )    // Insert edge
-        throw ( logic_error );
-    bool retrieveVertex ( const string& v, Vertex& vData ) const;
-                                                      // Get vertex
-    bool getEdgeWeight ( const string& v1, const string& v2, int& wt ) const
-        throw ( logic_error );                        // Get edge wt.
-    void removeVertex ( const string& v )                     // Remove vertex
-        throw ( logic_error );
-    void removeEdge ( const string& v1, const string& v2 )            // Remove edge
-        throw ( logic_error );
-    void clear ();                                    // Clear graph
+		// Graph manipulation operations
+		void insertVertex ( const Vertex& newVertex )            // Insert vertex
+			throw ( logic_error );
+		void insertEdge ( const string& v1, const string& v2, int wt )    // Insert edge
+			throw ( logic_error );
+		bool retrieveVertex ( const string& v, Vertex& vData ) const;
+		// Get vertex
+		bool getEdgeWeight ( const string& v1, const string& v2, int& wt ) const
+			throw ( logic_error );                        // Get edge wt.
+		void removeVertex ( const string& v )                     // Remove vertex
+			throw ( logic_error );
+		void removeEdge ( const string& v1, const string& v2 )            // Remove edge
+			throw ( logic_error );
+		void clear ();                                    // Clear graph
 
-    // Graph status operations
-    bool isEmpty () const;                            // Graph is empty
-    bool isFull () const;                             // Graph is full
+		// Graph status operations
+		bool isEmpty () const;                            // Graph is empty
+		bool isFull () const;                             // Graph is full
 
-    // Output the graph structure -- used in testing/debugging
-    void showStructure () const;
+		// Output the graph structure -- used in testing/debugging
+		void showStructure () const;
 
-    // Programming exercise operations
-    void showShortestPaths () const;
-    bool hasProperColoring() const;
-    bool areAllEven () const;
+		// Programming exercise operations
+		void showShortestPaths () const;
+		bool hasProperColoring() const;
+		bool areAllEven () const;
 
-  private:
+	private:
 
-    // Facilitator functions
-    int getIndex ( const string& v ) const;          // Converts vertex label to an
-                                               //   adjacency matrix index
-                                       
-    int getEdge ( int row, int col ) const;          // Get edge weight using
-                                               //   adjacency matrix indices
-    void setEdge ( int row, int col, int wt);  // Set edge weight using
-	 inline int toArr(int x, int y) const {return y*maxSize + x;}
-                                               //   adjacency matrix indices
-    // Data members
-    int maxSize,          // Maximum number of vertices in the graph
-        size;             // Actual number of vertices in the graph
-    Vertex *vertexList;   // Vertex list
-    int *adjMatrix;       // Adjacency matrix
+		// Facilitator functions
+		int getIndex ( const string& v ) const;          // Converts vertex label to an
+		//   adjacency matrix index
+		void getPaths() const;
+
+		int getEdge ( int row, int col ) const;          // Get edge weight using
+		//   adjacency matrix indices
+		void setEdge ( int row, int col, int wt);  // Set edge weight using
+		inline int toArr(int x, int y) const {return y*maxSize + x;}
+		//   adjacency matrix indices
+		// Data members
+		int maxSize,          // Maximum number of vertices in the graph
+			 size;             // Actual number of vertices in the graph
+		Vertex *vertexList;   // Vertex list
+		int *adjMatrix;       // Adjacency matrix
+		int *pathMatrix;
 
 };
 
